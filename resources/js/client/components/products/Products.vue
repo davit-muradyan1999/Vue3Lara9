33f4d15dcbaad5b77eda137b6cf2c59df5447e99
unknown <you@example.com>
2025-03-18 00:54:59 +0400

    17.03 home

--- resources/js/client/components/products/Products.vue
+++ resources/js/client/components/products/Products.vue
@@ -2,15 +2,36 @@
     <div>
         <h1 v-if="private">Private Product</h1>
         <h1 v-else>Product Category: {{ category.title }}</h1>
-        <div class="filters">
+        <div class="filters flex items-center justify-self-start gap-4">
             <p>Filter:</p>
-            <select id="small" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500">
-                <option selected>Choose a country</option>
-                <option value="US">United States</option>
-                <option value="CA">Canada</option>
-                <option value="FR">France</option>
-                <option value="DE">Germany</option>
-            </select>
+            <div class="w-full max-w-sm min-w-[200px]">
+                <div class="relative">
+                    <select
+                        class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                        <option value="brazil">Brazil</option>
+                        <option value="bucharest">Bucharest</option>
+                        <option value="london">London</option>
+                        <option value="washington">Washington</option>
+                    </select>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                </div>
+            </div>
+            <div class="w-full max-w-sm min-w-[200px]">
+                <div class="relative">
+                    <select
+                        class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded pl-3 pr-8 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-400 shadow-sm focus:shadow-md appearance-none cursor-pointer">
+                        <option value="brazil">Brazil</option>
+                        <option value="bucharest">Bucharest</option>
+                        <option value="london">London</option>
+                        <option value="washington">Washington</option>
+                    </select>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="h-5 w-5 ml-1 absolute top-2.5 right-2.5 text-slate-700">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                </div>
+            </div>
         </div>
         <ul>
             <li v-for="product in products" :key="product.id">
