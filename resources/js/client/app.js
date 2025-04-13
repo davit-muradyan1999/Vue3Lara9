cbe8f784f72ded6eb20838067afc7dc2acb96ebe
unknown <you@example.com>
2025-04-13 23:13:08 +0400

    Last Deploy Version 13.04 v12

--- resources/js/client/app.js
+++ resources/js/client/app.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import './../../assets/styles/globals.scss';
 
-import { Ziggy } from '@/ziggy'; // путь может быть другим
+import {ZiggyVue} from "ziggy-js"; // путь может быть другим
 import route from 'ziggy-js';
 
 import App from './components/App.vue';
@@ -9,6 +9,6 @@ import App from './components/App.vue';
 const app = createApp(App);
 
 app.config.globalProperties.route = (name, params, absolute) =>
-    route(name, params, absolute, Ziggy);
+    route(name, params, absolute, ZiggyVue);
 
 app.mount('#app');
